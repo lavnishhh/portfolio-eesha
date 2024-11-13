fd05ae2aa5c5c931f7cdb302cccf642f7123be15
Lavnish <lavnishdesigner@gmail.com>
2024-11-13 14:29:41 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <HomeScreen></HomeScreen>,
   },
-], { basename: "/portfolio-eesha" });
+],);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
