97f7e90555ba66ccc40a139ffc9ce3cacad97c72
Lavnish <lavnishdesigner@gmail.com>
2024-11-13 14:42:40 +0530

    removed nav

--- src/homescreen/home_screen.jsx
+++ src/homescreen/home_screen.jsx
@@ -34,11 +34,11 @@ function HomeScreen() {
 
     return (
         <>
-            <nav className="top-0 z-50 backdrop-blur-sm sticky border-b border-primary-500 justify-center flex-grow flex items-center pt-2 ps-2">
+            {/* <nav className="top-0 z-50 backdrop-blur-sm sticky border-b border-primary-500 justify-center flex-grow flex items-center pt-2 ps-2">
                 <div>
                     <button type="button" className="text-white bg-primary-700 hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Resume</button>
                 </div>
-            </nav>
+            </nav> */}
             <LandingPage callback={callback} />
             <ExperiencePage refProp={exp} />
             <ProjectsPage refProp={hrproj} />
